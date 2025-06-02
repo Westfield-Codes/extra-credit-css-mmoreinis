c6fd61738b2f9885d85faa77927d11ba1b2374b7
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-06-02 14:04:35 +0000

    FirstJS

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function setup(){
+    let place = document.getElementById("name");
+    let button = document.createElement("button");
+    button.innerHTML="Click to enter name";
+    button.addEventListener("click", addName);
+    place.appendChild(button);
+}
 
+function addName(){
+    let myName = prompt("Enter name here");
+    let place = document.getElementById("name");
+    place.innerHTML = "<h3>"+myName+"</h3>";
+}
