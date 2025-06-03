bc016ca6dfc52e957e18040a09c62647575d1426
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-06-03 14:03:42 +0000

    Get rounds

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ function setup(){
 }
 
 function addName(){
-    let myName = prompt("Enter name here");
+    let nameBox = document.getElementById("get-name");
+    let myName = nameBox.value;
     let place = document.getElementById("name");
     place.innerHTML = "<h3>"+myName+"</h3>";
+    let play = document.createElement("button");
+    play.innerHTML = "Play RPS";
+    play.addEventListener("click", howManyGames);
+    place.appendChild(play);
+    place.style.borderBottom = "none";
+}
+
+function howManyGames(){
+    let title = document.getElementById("title");
+    title.innerHTML = "RPS";
+    let subtitle = document.getElementById("subtitle");
+    subtitle.innerHTML = "How many rounds?";
 }
